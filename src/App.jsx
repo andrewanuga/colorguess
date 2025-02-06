9697924bf1f45c40afc3cc889df8e923da4fd27e
ANDREW <adakoleandrew21@gmail.com>
2025-02-06 13:29:55 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import WholeApp from './components/WholeApp'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <WholeApp />
+    </div>
+    </>
+  )
+}
+
+export default App
