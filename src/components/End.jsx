eec471ba1f904ecbd1e0af043e9cd3c4f41c4594
ANDREW <adakoleandrew21@gmail.com>
2025-02-09 13:44:02 +0100

    npm run build

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -16,6 +16,7 @@ const End = ({ openend, setOpenend, score }) => {
         height="24px" 
         viewBox="0 -960 960 960" 
         width="24px" 
+        
         fill="#121212">
         <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
       </svg></p>
